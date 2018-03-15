9845c0645269d7fb6285be345ae66c08dfebf188
Cain <cain.hall98@gmail.com>
2018-03-15 15:27:38 +1100

    Create room services, basic join/create room requests

--- src/components/Lobby.vue
+++ src/components/Lobby.vue
@@ -1,13 +1,55 @@
 <template>
-  <h1>
-    Lobby
-  </h1>
+  <div class="room-container">
+    <h1>
+      Lobby
+    </h1>
+
+    <div class="">
+      <input v-model="roomId" type="text" name="room" value="" placeholder="Enter a room id" />
+      <button type="button" @click="joinRoom" name="button" :disabled="roomId.length == 0">Go!</button>
+      <br />
+      <small>{{responseMessage}}</small>
+
+    </div>
+    <br />
+    OR <br /> <br />
+    <button type="button" name="button" @click="createRoom">Create Room</button>
+    <br />
+    <small>eg: "5aa89329ed45ec7a4a1a8205"</small>
+  </div>
 </template>
 
 <script>
+import RoomService from '../services/room.service'
+
 export default {
+  data () {
+    return {
+      roomId: '',
+      responseMessage: ''
+    }
+  },
+  methods: {
+    joinRoom: function () {
+      RoomService.join(this.roomId).then(res => {
+        console.log(res.data.userId)
+      }).catch(e => { this.responseMessage = {...e}.response.data.message })
+    },
+    createRoom: function () {
+      RoomService.create().then(res => {
+        console.log(res.data.userId)
+      }).catch(e => { this.responseMessage = {...e}.response.data.message })
+    }
+  }
 }
 </script>
 
-<style lang="css">
+<style lang="scss" scoped>
+.room-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh
+}
 </style>
---
+++ src/services/room.service.js
@@ -0,0 +1,7 @@
+import axios from 'axios'
+import { API_URL } from '../config'
+
+export default {
+  join: (id) => axios.get(`${API_URL}/room/join/${id}`),
+  create: () => axios.get(`${API_URL}/room/create`)
+}
